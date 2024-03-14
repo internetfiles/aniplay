35e03d45c4bf05ccab41a231bdda40f41ff93798
Robert Jarrett <howtotech77@gmail.com>
2024-03-14 16:18:36 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -74,7 +74,7 @@ function Footer() {
                                         <Link href="/dmca" className="hover:text-white"> DMCA</Link>
                                     </li>
                                     <li>
-                                        <Link href="https://ko-fi.com/luckyhv" target='_blank' className="hover:text-white !font-semibold !text-[0.8rem]">Donate</Link>
+                                        <Link href="https://ko-fi.com/makima" target='_blank' className="hover:text-white !font-semibold !text-[0.8rem]">Donate</Link>
                                     </li>
                                 </ul>
                             </div>
@@ -83,7 +83,7 @@ function Footer() {
                 </div>
                 <div className='bg-tersier border-t border-white/5 mt-2'></div>
                 <div className="mx-auto w-full lg:max-w-[83%] lg:flex lg:items-center lg:justify-between lg:text-[0.8rem] text-[0.7rem] text-[#ffffffb2] py-3">
-                    <span className="sm:text-center ms-5 lg:ms-0">© {year} <Link href="/" className="hover:text-white">ANIPLAY™</Link> | Made by <span className="font-bold" >Luckyhv</span>
+                    <span className="sm:text-center ms-5 lg:ms-0">© {year} <Link href="/" className="hover:text-white">MAKIMA™</Link> | Made by <span className="font-bold" >MAKIMA</span>
                     </span>
                     <div className="flex mt-4 lg:justify-center lg:mt-0">
                         <Link href="https://github.com/Luckyhv/aniplay" target="_blank" className=" hover:text-gray-900 dark:hover:text-white ms-5 lg:ms-0">
@@ -92,7 +92,7 @@ function Footer() {
                             </svg>
                             <span className="sr-only">GitHub account</span>
                         </Link>
-                        <Link href="https://discord.gg/aW5P4W94ZS" target="_blank" className=" hover:text-gray-900 dark:hover:text-white ms-5">
+                        <Link href="https://discord.gg/#" target="_blank" className=" hover:text-gray-900 dark:hover:text-white ms-5">
                             <svg className="w-[22px] h-[22px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
                                 <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
                             </svg>

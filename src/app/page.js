1dedec9a77e0fa13d25710ce4d6440967949ca5a
Robert Jarrett <howtotech77@gmail.com>
2024-03-23 12:26:52 -0600

    updated top 10 anime

--- src/app/page.js
+++ src/app/page.js
@@ -85,7 +85,7 @@ async function Home() {
           viewport={{ once: true }}
         >
           <div className='lg:flex lg:flex-row justify-between lg:gap-20'>
-            <VerticalList data={top100data} mobiledata={seasonaldata} id="Top 100 Anime" />
+            <VerticalList data={top100data} mobiledata={seasonaldata} id="Top 10 Anime" />
             <VerticalList data={seasonaldata} id="Seasonal Anime" />
           </div>
         </MotionDiv>

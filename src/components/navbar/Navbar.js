a61373d5eaea20524c8c448bdf8ee3f03f3cbe50
Robert Jarrett <howtotech77@gmail.com>
2024-04-13 16:01:50 -0600

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -116,7 +116,6 @@ function Navbarcomponent({ home = false }) {
                     <Link href="/anime/catalog" className={styles.navItem}>Catalog</Link>
                     <Link href="/anime/catalog?sortby=TRENDING_DESC" className={styles.navItem}>Trending</Link>
                     <Link href="/anime/catalog?format=MOVIE" className={styles.navItem}>Movies</Link>
-                    <Link href="/search?year=2024" className={styles.navItem}>Latest</Link>
                 </div>
             </div>
             <div className={styles.navright}>
